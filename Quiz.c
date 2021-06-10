7d011adf13db1e4ef54fdaa5cc2e8118727925b1
Samiur Prapon <samiur.prapon@gmail.com>
2021-06-10 12:19:20 +0600

    Indentation issues fixed

--- Quiz.c
+++ Quiz.c
@@ -1,151 +1,121 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int x, i, score=0, temp_score;
-    char a;
+	
+	int i;
+	int score = 0;
+	int temp_score;
+	char answer;
 
-    for (i=1; i<=4; i++)
-    {
-        switch (i)
-    {
-case 1:
-        printf("\n1)What is the first progaram?\n");
-        printf("A.Hello world\tB.Hello Human\nC.Hello Progarm\tD.Hello\n");
-        scanf(" %c", &a);
-        if (a=='A'  || a=='a')
-        {
-            printf("\nCORRECT!");
-            score++;
-        }
-        else
-        {
-            printf("\nINCORRECT! CORRECT ANSWER:A");
+	for (i = 1; i <= 4; i++) {
+		switch (i) {
+		case 1:
+			printf("\n1) What is the first progaram?\n");
+			printf("A.Hello world\tB.Hello Human\nC.Hello Progarm\tD.Hello\n");
+			scanf(" %c", &answer);
 
-        }
-        break;
+			if (answer == 'A' || answer == 'a') {
+				printf("\nCORRECT!\n");
+				score++;
+			} else {
+				printf("\nINCORRECT! CORRECT ANSWER: A\n");
+			}
 
+			break;
 
+		case 2:
+			printf("\n2) What is considered computer's short term memory?\n");
+			printf("A.RAM\tB.ROM\nC.REM\tD.CD-ROM\n");
+			scanf(" %c", &answer);
+			
+			if (answer == 'A' || answer == 'a') {
+				printf("\nCORRECT!\n");
+				score++;
+			} else {
+				printf("\nINCORRECT! CORRECT ANSWER: A\n");
+			}
+			break;
 
-case 2:
+		case 3:
+			printf("\n3) The correct declaration of the integer named 'i' is:\n");
+			printf("A.int i;\tB.i=int;\nC.integer=i;\tD.int=i;\n");
+			scanf(" %c", &answer);
+			
+			if (answer == 'A' || answer == 'a') {
+				printf("\nCORRECT!\n");
+				score++;
+			} else {
+				printf("\nINCORRECT! CORRECT ANSWER: A\n");
+			}
 
-        printf("\n2)What is considered computer's short term memory?\n");
-                printf("A.RAM\tB.ROM\nC.REM\tD.CD-ROM\n");
-                scanf(" %c", &a);
-             if (a=='A'  || a=='a')
-        {
-            printf("\nCORRECT!");
-            score++;
-        }
-        else
-        {
-            printf("\nINCORRECT! CORRECT ANSWER:A");
+			break;
 
-        }
-        break;
+		case 4:
+			printf("\n4) Which of the following data types are ideal for numbers?\n");
+			printf("A.float\tB.char\nC.int\tD.A & C Both\n");
+			scanf(" %c", &answer);
+			
+			if (answer == 'D' || answer == 'd') {
+				printf("\nCORRECT!\n");
+				score++;
+			} else {
+				printf("\nINCORRECT! CORRECT ANSWER: D\n");
+			}
 
+			break;
+		}
+	}
 
-case 3:
+	printf("\nRound 1 Score:%d\n", score);
 
-        printf("\n3)The correct declaration of the integer named 'i' is:\n");
-                printf("A.int i;\tB.i=int;\nC.integer=i;\tD.int=i;\n");
-                scanf(" %c", &a);
-             if (a=='A'  || a=='a')
-        {
-            printf("\nCORRECT!");
-            score++;
-        }
-        else
-        {
-            printf("\nINCORRECT! CORRECT ANSWER:A");
+	if (score >= 3) {
+		printf("\nYou have completed Round 1! :) \nDo you want to play Round 2? (Y/N)\n");
+		scanf(" %c", &answer);
+	}
+	else {
+		printf("\nYou have Failed!\n");
+	}
+	
+	temp_score = score;
+	score = 0;
+	
+	if (answer == 'Y' || answer == 'y') {
 
-        }
-        break;
+		for (i = 1; i <= 2; i++) {
 
+			switch (i) {
 
+			case 1:
+				printf("\n1) Each time that a loop <loops> is known as a(n):\n");
+				printf("A.count\t\tB.increment\nC.iteration\tD.none of these\n");
+				scanf(" %c", &answer);
+				
+				if (answer == 'C' || answer == 'c') {
+					printf("\nCORRECT!\n");
+					score++;
+				} else {
+					printf("\nINCORRECT! CORRECT ANSWER: C\n");
+				}
 
-case 4:
+				break;
 
-        printf("\n4)Which of the following data types are ideal for numbers?\n");
-                printf("A.float\tB.char\nC.int\tD.A & C Both\n");
-                scanf(" %c", &a);
-             if (a=='D'  || a=='d')
-        {
-            printf("\nCORRECT!");
-            score++;
-        }
-        else
-        {
-            printf("\nINCORRECT! CORRECT ANSWER:D");
+			case 2:
+				printf("\n2) Each time that a loop <loops> is known as a(n):\n");
+				printf("A.count\t\tB.increment\nC.iteration\tD.none of these\n");
+				scanf(" %c", &answer);
+				
+				if (answer == 'C' || answer == 'c') {
+					printf("\nCORRECT!\n");
+					score++;
+				} else {
+					printf("\nINCORRECT! CORRECT ANSWER: C\n");
+				}
 
-        }
-        break;
-    }
+				break;
+			}
+		}
+	}
 
-    }
-
-
-printf("\nRound 1 Score:%d\n", score);
-
-
-
-if (score>=3)
-   {
-    printf("\nYou have completed Round 1! :) \nDo you want to play Round 2? (Y/N)\n");
-   scanf(" %c", &a);
-   }
-   else{
-   printf("\nYou have Failed!\n");
-   }
-temp_score=score;
-score=0;
-   if (a=='Y' || a=='y')
-   {
-
-    for (i=1; i<=2; i++)
-    {
-
-        switch(i)
-        {
-
-case 1:
-        printf("\n1)Each time that a loop <loops> is known as a(n):\n");
-        printf("A.count\tB.increment\nC.iteration\tD.none of these\n");
-        scanf(" %c", &a);
-        if (a=='C'  || a=='c')
-        {
-            printf("\nCORRECT!");
-            score++;
-        }
-        else
-        {
-            printf("\nINCORRECT! CORRECT ANSWER:C");
-
-        }
-        break;
-
-
-case 2:
-        printf("\n1)Each time that a loop <loops> is known as a(n):\n");
-        printf("A.count\tB.increment\nC.iteration\tD.none of these\n");
-        scanf(" %c", &a);
-        if (a=='C'  || a=='c')
-        {
-            printf("\nCORRECT!");
-            score++;
-        }
-        else
-        {
-            printf("\nINCORRECT! CORRECT ANSWER:C");
-
-        }
-        break;
-
-
-        }
-
-    }
-   }
-
-printf("Round 2 Score: %d", score);
+	printf("Round 2 Score: %d\n", score);
 }
